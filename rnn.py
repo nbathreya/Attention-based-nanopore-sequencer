b80c14ddce537d88fd837babd6532d0f5e50bae9
nbathreya <nbathreya@gmail.com>
2018-12-14 16:52:49 -0600

    Update rnn.py

--- rnn.py
+++ rnn.py
@@ -1,10 +1,7 @@
 # Copyright 2017 The Chiron Authors. All Rights Reserved.
-#
-#This Source Code Form is subject to the terms of the Mozilla Public
-#License, v. 2.0. If a copy of the MPL was not distributed with this
-#file, You can obtain one at http://mozilla.org/MPL/2.0/.
-#
-#Created on Fri Apr 21 03:46:35 2017
+#Ref: https://github.com/haotianteng/Chiron
+
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
